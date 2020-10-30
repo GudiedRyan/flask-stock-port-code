88688e0d96fbe900cfd8360e5a7b39e89a0de770
Ryan Guide <ryan.guide@comcast.net>
2020-10-30 14:26:58 -0400

    Finished part 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from flask import current_app
-from project import create_app
+from project import create_app, database
 
 
 @pytest.fixture(scope='module')
@@ -10,7 +10,14 @@ def test_client():
 
     # Create a test client using the Flask appl config made for testing
     with flask_app.test_client() as testing_client:
+        # Establish an application context before accessing the logger and database
         with flask_app.app_context():
-            current_app.logger.info('In the test_client() fixture...')
-        
-        yield testing_client
+            flask_app.logger.info('Creating database tables in test_client fixture...')
+
+            # Create the database and the database table(s)
+            database.create_all()
+
+        yield testing_client  # this is where the testing happens!
+
+        with flask_app.app_context():
+            database.drop_all()
